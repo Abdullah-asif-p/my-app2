f98146a9b704b5fad8c81c063361cdad9de98933
Abdullah-asif-p <abdullahasifmf007+github@gmail.com>
2024-01-26 11:53:20 +0500

    First commit

--- src/components/veiws/products.tsx
+++ src/components/veiws/products.tsx
@@ -8,12 +8,12 @@ export default function Products() {
         </span>
         <h2 className=" text-4xl">Our Promotions Events</h2>
       </div>
-      <div className="flex justify-between gap-2 ">
-        <div className=" flex-[2] flex-col gap-1 ">
-          <div className=" bg-slate-400  justify-between item-center">
-            <div className="content">
-              <h3>
-                GET UP TO <span>60%</span>
+      <div className="flex justify-between gap-8 ">
+        <div className="flex flex-[2] flex-col gap-4 ">
+          <div className=" bg-[#d6d6d8]  flex justify-between items-center px-8 ">
+            <div className="px-5  box-border">
+              <h3 className=" text-3xl font-bold">
+                GET UP TO <span className=" font-extrabold text-4xl ">60%</span>
               </h3>
               <p>For the summer season</p>
             </div>
@@ -22,23 +22,24 @@ export default function Products() {
               alt="img"
               width={260}
               height={260}
+              className=" w-[260px] h-[200px]"
             />
           </div>
-          <div className="flex flex-col justify-center items-center text-white bg-[#212121]">
-            <h3 className=" text-4xl font-extrabold">GET 30% Off</h3>
+          <div className="flex flex-col justify-center items-center text-white bg-[#212121] pt-12 pb-8 pr-8 ">
+            <h3 className=" text-4xl font-extrabold mb-4 ">GET 30% Off</h3>
             <p>USE PROMO CODE</p>
-            <button className=" bg-gray-500 p-1 px-4 rounded-lg">
+            <button className=" text-lg font-bold tracking-[0.25rem] mt-1 bg-[#474747] py-2 px-10 rounded-lg">
               DINEWEEKENDSALE
             </button>
           </div>
         </div>
-        <div className="bg-slate-500 ">
+        <div className="bg-[#efe1c7] ml-4">
           <div className="">
-            <div className="">
+            <div className="p-5">
               <p>Flex Sweatshirt</p>
               <div className="">
-                <span>$100.00</span>
-                <span>$75.00</span>
+                <span className="line-through font-light">$100.00</span>
+                <span className="text-lg ">$75.00</span>
               </div>
             </div>
             <Image
@@ -49,13 +50,13 @@ export default function Products() {
             />
           </div>
         </div>
-        <div className="bg-slate-500 ">
+        <div className="bg-[#d7d7d9] ">
           <div className="">
-            <div className="">
+            <div className="p-5">
               <p>Flex Sweatshirt</p>
               <div className="">
-                <span>$100.00</span>
-                <span>$75.00</span>
+                <span className="line-through font-light">$225.00</span>
+                <span className="text-lg ">$190.00</span>
               </div>
             </div>
             <Image
